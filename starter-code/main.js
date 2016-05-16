ee8968a08dd0d1645694c36b4c7b425b2ce4720c
Anna Sanderson <anna.sanderson@csuglobal.edu>
2016-05-16 00:39:12 -0400

    finished product

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,4 @@
-var cardOne = 'queen';
+/*var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
@@ -8,13 +8,42 @@ var cardFour = 'king';
 } else {
 	alert('Sorry, try again!');
 }*/
+var cards = ['queen','queen','king','king'];
+var cardsInPlay =[];
+
 var board = document.getElementById('game-board');
 
 var createBoard = function() {
 for (i=0; i < cards.length; i++) {
 	var cardElement = document.createElement('div');
 	cardElement.className = 'card';
+	cardElement.setAttribute ('data-card', cards[i]);
+ 	cardElement.addEventListener ('click', isTwoCards);
+
 	board.appendChild(cardElement);
 	}
 };
+ 
+var isMatch = function(){
+	if (cards[0]===cards[1]) {
+		alert ("You found a match!");
+	} else if (cards [2]===cards[3]){
+		alert ("You found a match!");
+	}else {
+		alert("Sorry, try again!");
+	}
+};
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute ('data-card')==='king') {
+		cardElement.innerHTML ="<img src="http://i.imgur.com/09wsiBYt.png"/>";
+	} else {
+		cardElement.innerHTML = "<img src="http://i.imgur.com/qS50KAAt.png"/>";
+	}
+	if (cardsInPlay.length===2) {
+		isMatch(cardsInPlay);
+		cardsInPlay =[];
+	}
+}
 
